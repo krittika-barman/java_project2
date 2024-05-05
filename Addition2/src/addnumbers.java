9d95385bb17da726a97b14fabae752e1bac0c399
krittika-barman <135047917+krittika-barman@users.noreply.github.com>
2024-05-05 15:57:22 +0530

    c6-from remote

--- Addition2/src/addnumbers.java
+++ Addition2/src/addnumbers.java
@@ -3,8 +3,8 @@ public class addnumbers {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 15;
-		int b = 16;
+		int a = 200;
+		int b = 100;
 		int c = a+b;
 		System.out.println("sum of a and b is :"+ c);
 	}
